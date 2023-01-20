04eb6e3b9f20f27d29a4f38996e84930dd51ee4f
Luqman Hakim <luqman.hakim.pens@gmail.com>
2023-01-20 10:11:34 +0700

    Add project files.

---
+++ timer_and_counter/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace timer_and_counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxTimerDecision = new System.Windows.Forms.TextBox();
+            this.textBoxSubmitted = new System.Windows.Forms.TextBox();
+            this.textBoxSkipped = new System.Windows.Forms.TextBox();
+            this.textBoxAdt = new System.Windows.Forms.TextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.buttonSkip = new System.Windows.Forms.Button();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxTimerDecision
+            // 
+            this.textBoxTimerDecision.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTimerDecision.Location = new System.Drawing.Point(7, 82);
+            this.textBoxTimerDecision.Name = "textBoxTimerDecision";
+            this.textBoxTimerDecision.Size = new System.Drawing.Size(90, 32);
+            this.textBoxTimerDecision.TabIndex = 15;
+            this.textBoxTimerDecision.Text = "00:00:00";
+            this.textBoxTimerDecision.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxTimerDecision.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxTimerDecision_KeyDown);
+            this.textBoxTimerDecision.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBoxTimerDecision_MouseDoubleClick);
+            // 
+            // textBoxSubmitted
+            // 
+            this.textBoxSubmitted.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSubmitted.Location = new System.Drawing.Point(103, 82);
+            this.textBoxSubmitted.Name = "textBoxSubmitted";
+            this.textBoxSubmitted.Size = new System.Drawing.Size(44, 32);
+            this.textBoxSubmitted.TabIndex = 17;
+            this.textBoxSubmitted.Text = "000";
+            this.textBoxSubmitted.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxSubmitted.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSubmitted_KeyDown);
+            // 
+            // textBoxSkipped
+            // 
+            this.textBoxSkipped.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSkipped.Location = new System.Drawing.Point(103, 44);
+            this.textBoxSkipped.Name = "textBoxSkipped";
+            this.textBoxSkipped.Size = new System.Drawing.Size(43, 32);
+            this.textBoxSkipped.TabIndex = 18;
+            this.textBoxSkipped.Text = "000";
+            this.textBoxSkipped.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxSkipped.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSkipped_KeyDown);
+            // 
+            // textBoxAdt
+            // 
+            this.textBoxAdt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAdt.Location = new System.Drawing.Point(7, 44);
+            this.textBoxAdt.Name = "textBoxAdt";
+            this.textBoxAdt.Size = new System.Drawing.Size(90, 32);
+            this.textBoxAdt.TabIndex = 19;
+            this.textBoxAdt.Text = "00.00";
+            this.textBoxAdt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonReset.BackgroundImage = global::timer_and_counter.Properties.Resources.reset_icon;
+            this.buttonReset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.Location = new System.Drawing.Point(47, 8);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(34, 32);
+            this.buttonReset.TabIndex = 7;
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSubmit.BackgroundImage = global::timer_and_counter.Properties.Resources.submiticon;
+            this.buttonSubmit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonSubmit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSubmit.Location = new System.Drawing.Point(153, 82);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(34, 32);
+            this.buttonSubmit.TabIndex = 6;
+            this.buttonSubmit.UseVisualStyleBackColor = false;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // buttonSkip
+            // 
+            this.buttonSkip.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSkip.BackgroundImage = global::timer_and_counter.Properties.Resources.skip_icon;
+            this.buttonSkip.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonSkip.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSkip.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSkip.Location = new System.Drawing.Point(153, 44);
+            this.buttonSkip.Name = "buttonSkip";
+            this.buttonSkip.Size = new System.Drawing.Size(34, 32);
+            this.buttonSkip.TabIndex = 5;
+            this.buttonSkip.UseVisualStyleBackColor = false;
+            this.buttonSkip.Click += new System.EventHandler(this.buttonSkip_Click);
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonStartStop.BackgroundImage = global::timer_and_counter.Properties.Resources.play_icon;
+            this.buttonStartStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonStartStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartStop.Location = new System.Drawing.Point(7, 8);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(34, 32);
+            this.buttonStartStop.TabIndex = 0;
+            this.buttonStartStop.UseVisualStyleBackColor = false;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(159, 8);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(28, 13);
+            this.labelVersion.TabIndex = 20;
+            this.labelVersion.Text = "v0.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(197, 117);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.textBoxAdt);
+            this.Controls.Add(this.textBoxSkipped);
+            this.Controls.Add(this.textBoxSubmitted);
+            this.Controls.Add(this.textBoxTimerDecision);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.buttonSkip);
+            this.Controls.Add(this.buttonStartStop);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ADT Tracker";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.Button buttonSkip;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.TextBox textBoxTimerDecision;
+        private System.Windows.Forms.TextBox textBoxSubmitted;
+        private System.Windows.Forms.TextBox textBoxSkipped;
+        private System.Windows.Forms.TextBox textBoxAdt;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelVersion;
+    }
+}
+
---
+++ timer_and_counter/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using timer_and_counter.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace timer_and_counter
+{
+    public partial class Form1 : Form
+    {
+        String appVersion = "v1.1";
+        DateTime tt;
+        DateTime timerLastDecision;
+        DateTime timerTotalReview;
+        DateTime startTime;
+        DateTime timerDecision;
+        
+        TimeSpan td,last_td;
+
+        private static System.Timers.Timer aTimer;
+
+        bool timerRunning = false, timerstarted = false;
+        
+        UInt32 submitted,skipped;
+        double adt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelVersion.Text = appVersion;
+            timer1.Stop();
+            SetTimer();
+
+            timerDecision = Settings.Default.timeDecision;
+            timerTotalReview = Settings.Default.timeReview;
+            timerLastDecision = Settings.Default.timeLastDecision;
+            skipped = Settings.Default.skip;
+            submitted = Settings.Default.submit;
+            adt = Settings.Default.adt;
+
+            textBoxTimerDecision.Text = timerDecision.TimeOfDay.ToString();
+            textBoxSubmitted.Text = submitted.ToString();
+            textBoxSkipped.Text = skipped.ToString();
+            textBoxAdt.Text = adt.ToString();
+        }
+
+        private void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(1000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            aTimer.Stop();
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            //tt = tt.AddSeconds(1);
+            //timerDecision = timerDecision.AddSeconds(1);
+
+            TimeSpan timediff = DateTime.Now - startTime;
+            startTime = DateTime.Now;
+            timerDecision = timerDecision.Add(timediff);
+
+            Console.Write(timediff.ToString());
+            Console.Write(" - ");
+            Console.WriteLine(timerDecision.TimeOfDay.ToString());
+            //Console.WriteLine("t  = {0:HH:mm:ss.fff}", timerDecision);
+
+            if (textBoxTimerDecision.InvokeRequired)
+            {
+                textBoxTimerDecision.BeginInvoke(new MethodInvoker(() => textBoxTimerDecision.Text = timerDecision.TimeOfDay.ToString()));
+            }
+            save_state();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //timerDecision = timerDecision.AddSeconds(1);
+
+            DateTime now = DateTime.Now;
+            TimeSpan timediff = now - startTime;
+            timerDecision = timerDecision.Add(timediff);
+            textBoxTimerDecision.Text = timerDecision.TimeOfDay.ToString();
+            
+            save_state();
+        }
+
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if (!timerRunning)
+            {
+                //timer1.Start();
+                //if(timerstarted)
+                startTime = DateTime.Now;
+                aTimer.Start();
+                timerRunning = true;
+                buttonStartStop.BackgroundImage = global::timer_and_counter.Properties.Resources.pause_icon;
+                SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\Windows Unlock.wav");
+                simpleSound.Play();
+            }
+            else
+            {
+                //timer1.Stop();
+                last_td = td;
+                aTimer.Stop();
+                timerRunning = false;
+                buttonStartStop.BackgroundImage = global::timer_and_counter.Properties.Resources.play_icon;
+                SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\Windows Pop-up Blocked.wav");
+                simpleSound.Play();
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Reset?", "Reset?", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (res == DialogResult.OK)
+            {
+                timerTotalReview = timerDecision = DateTime.MinValue;
+                textBoxTimerDecision.Text = timerTotalReview.TimeOfDay.ToString();
+
+                adt = 0;
+                textBoxAdt.Text = adt.ToString();
+
+                submitted = skipped = 0;
+                textBoxSkipped.Text = skipped.ToString();
+                textBoxSubmitted.Text = submitted.ToString();
+
+                save_state();
+            }            
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            submitted++;
+            textBoxSubmitted.Text = submitted.ToString();
+            timerLastDecision = timerDecision;
+
+            adt = Math.Round(getTotalSeconds(timerDecision) / submitted, 2);
+            textBoxAdt.Text = adt.ToString();
+
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\Speech On.wav");
+            simpleSound.Play();
+        }
+
+        private void buttonSkip_Click(object sender, EventArgs e)
+        {
+            skipped++;
+            textBoxSkipped.Text = skipped.ToString();
+            timerDecision = timerLastDecision;
+            textBoxTimerDecision.Text = timerDecision.TimeOfDay.ToString();
+
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\Windows Balloon.wav");
+            simpleSound.Play();
+        }
+
+        private void buttonConvertToSecond_Click(object sender, EventArgs e)
+        {
+            textBoxTimerDecision.Text = getTotalSeconds(timerDecision).ToString();
+        }
+
+        private void textBoxTimerDecision_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                TimeSpan ts = TimeSpan.Parse(textBoxTimerDecision.Text);
+                timerDecision = timerDecision.Date + ts;
+                MessageBox.Show(timerDecision.TimeOfDay.ToString());
+            }
+        }
+
+        private void textBoxSubmitted_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                submitted = Convert.ToUInt32(textBoxSubmitted.Text);
+            }
+        }
+
+        private void textBoxSkipped_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                skipped = Convert.ToUInt32(textBoxSkipped.Text);
+            }
+        }
+
+        private void textBoxTimerDecision_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            getTotalSeconds(timerDecision);
+            textBoxTimerDecision.Text = getTotalSeconds(timerDecision).ToString();
+        }
+
+        private static double getTotalSeconds(DateTime date)
+        {
+            DateTime origin = DateTime.MinValue;
+            TimeSpan diff = date - origin;
+            return Math.Floor(diff.TotalSeconds);
+        }
+
+        private void save_state()
+        {
+            Settings.Default.timeDecision = timerDecision;
+            Settings.Default.timeReview = timerTotalReview;
+            Settings.Default.timeLastDecision = timerLastDecision;
+            Settings.Default.skip = skipped;
+            Settings.Default.submit = submitted;
+            Settings.Default.adt = adt;
+            Settings.Default.Save();
+        }
+    }
+}
